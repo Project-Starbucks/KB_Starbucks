7282051f073c78072e659742a1c38f6a5b011bdf
JOLLA99 <yoon913612@sch.ac.kr>
2024-05-16 09:08:32 +0900

    Modify_products

--- js/products.js
+++ js/products.js
@@ -1,42 +1,48 @@
 window.onload = function(){
-    let imgArr = ['../images/coffee1.jpeg', '../images/coffee2.jpeg', '../images/coffee3.jpeg',
-        '../images/coffee4.jpeg', '../images/coffee5.jpeg', '../images/coffee6.jpeg',
-        '../images/coffee7.jpeg', '../images/coffee8.jpeg', '../images/coffee9.jpeg',
-        '../images/coffee10.jpeg', '../images/coffee11.jpeg', '../images/coffee12.jpeg'
-    ]
-    let titleArr = ['씨솔트 카라멜 콜드 브루', '나이트로 바닐라 크림', '나이트로 콜드 브루', 
-        '돌체 콜드 브루', '리저브 나이트로', '리저브 콜드 브루', 
-        '민트 콜드 브루', '바닐라 크림 콜드 브루', '시그니처 더 블랙 콜드 브루', 
-        '여수 윤슬 헤이즐넛 콜드브루', '오트 콜드 브루', '제주 비자림 콜드 브루'
-    ]
+    const arr = [{code:'001', name:'씨솔트 카라멜 콜드 브루', img_src:'coffee1.jpeg'},
+                {code:'002', name:'나이트로 바닐라 크림', img_src:'coffee2.jpeg'},
+                {code:'003', name:'돌체 콜드 브루', img_src:'coffee3.jpeg'},
+                {code:'004', name:'리저브 나이트로', img_src:'coffee4.jpeg'},
+                {code:'005', name:'리저브 콜드 브루', img_src:'coffee5.jpeg'},
+                {code:'006', name:'민트 콜드 브루', img_src:'coffee6.jpeg'},
+                {code:'007', name:'바닐라 크림 콜드 브루', img_src:'coffee7.jpeg'},
+                {code:'008', name:'시그니처 더 블랙 콜드 브루', img_src:'coffee8.jpeg'},
+                {code:'009', name:'여수 윤슬 헤이즐넛 콜드브루', img_src:'coffee9.jpeg'},
+                {code:'010', name:'오트 콜드 브루', img_src:'coffee10.jpeg'},
+                {code:'011', name:'제주 비자림 콜드 브루', img_src:'coffee11.jpeg'},
+                {code:'012', name:'나이트로 콜드 브루', img_src:'coffee12.jpeg'}
+            ]
 
-    let bodyObj = document.querySelector('body')
-    let gridObj
+    let arr_len = arr.length;
+    let parentObj = document.querySelector("body")
+    let containerDiv
 
-    for (let i = 0; i < 12; i++){
-        if (i % 4 == 0){
-            gridObj = document.createElement('div')
-            gridObj.setAttribute('class', 'g')
-            bodyObj.appendChild(gridObj)
+    for(let i =0; i<=arr_len; i++){
+        if(i % 4 ==0){
+            containerDiv = document.createElement("div");
+            containerDiv.setAttribute('class', 'g')
+            parentObj.appendChild(containerDiv)
         }
-
-        let divObj = document.createElement('div')
-        divObj.setAttribute('id', 'g' + (i + 1))
-
-        let imgObj = document.createElement('img')
-        imgObj.setAttribute('src', imgArr[i])
         
-        let brObj = document.createElement('br')
+        let divObj = document.createElement('div')
+        divObj.setAttribute('id', 'g'+(i+1))
 
-        let titleObj = document.createElement('a')
-        let textObj2 = document.createTextNode(titleArr[i])
-        titleObj.appendChild(textObj2)
+        let imgElement = document.createElement("img")
+        imgElement.setAttribute("src", "../images/coffee" + (i+1) + ".jpeg")
+        imgElement.setAttribute("alt", arr[i].name)
+        divObj.appendChild(imgElement)
 
-        gridObj.appendChild(divObj)
-        divObj.appendChild(imgObj)
+        let brObj = document.createElement("br")
         divObj.appendChild(brObj)
-        divObj.appendChild(titleObj)
+
+        let textElement = document.createElement("a")
+        textElement.textContent = arr[i].name
+        divObj.appendChild(textElement)
+
+        containerDiv.appendChild(divObj)
     }
+    //-----------------------------//
+
 
     // --- 상품 click event handler START ---
     const tdObjs = document.querySelectorAll('div.g>div')
